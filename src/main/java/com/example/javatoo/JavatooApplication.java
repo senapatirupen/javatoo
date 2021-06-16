efdc92724700df92dd01c3ed5e04d695c62d75a0
senapatirupen <senapatirupen1986@gmail.com>
2021-06-16 11:57:22 +0530

    Initial commit

---
+++ src/main/java/com/example/javatoo/JavatooApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javatoo/JavatooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
