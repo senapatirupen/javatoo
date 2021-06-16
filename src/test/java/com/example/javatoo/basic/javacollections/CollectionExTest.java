caa12b8201415a63e152297164fffd9eae8256ef
senapatirupen <senapatirupen1986@gmail.com>
2021-06-16 13:56:31 +0530

    added Design Patterns

--- src/test/java/com/example/javatoo/basic/javacollections/CollectionExTest.java
+++ src/test/java/com/example/javatoo/basic/javacollections/CollectionExTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 @Slf4j
 public class CollectionExTest {
 //    @BeforeAll
@@ -30,5 +33,38 @@ public class CollectionExTest {
         listOfNames.remove("E");
         listOfNames.remove(2);
         log.info("display the content: " + listOfNames);
+    }
+
+    @Test
+    public void arrayListToArray(){
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        Integer[] numbersArray = new Integer[numbers.size()];
+        numbersArray = numbers.toArray(numbersArray);
+        //sum of the array
+        Integer sum = 0;
+        for(Integer n: numbersArray) sum += n;
+        log.info("display the content: " + numbers);
+        log.info("sum of integers: "+ sum);
+    }
+
+    @Test
+    public void linkedListTest(){
+        LinkedList<String> names = new LinkedList<String>();
+        names.add("B");
+        names.add("C");
+        names.add("D");
+        names.addFirst("A");
+        names.addLast("E");
+        log.info("display the content: " + names);
+        names.add(1, "A1");
+        names.set(3, "B1");
+        log.info("display the content: " + names);
+        names.remove(1);
+        names.removeLast();
+        names.removeFirst();
+        log.info("display the content: " + names);
     }
 }
