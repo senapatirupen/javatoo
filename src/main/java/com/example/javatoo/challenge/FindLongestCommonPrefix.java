79da48ddb7246f4de34a018a55679a3b02dc006b
senapatirupen <senapatirupen1986@gmail.com>
2021-07-25 20:10:58 +0530

    added java coding challenges

---
+++ src/main/java/com/example/javatoo/challenge/FindLongestCommonPrefix.java
@@ -0,0 +1,38 @@
+package com.example.javatoo.challenge;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FindLongestCommonPrefix {
+    private static String[] texts = {"abc", "abcd", "abcde", "ab", "abcd", "abcdef"};
+
+    public static void main(String[] args) {
+        log.info(new FindLongestCommonPrefix().findLongestCommonPrefixV1(texts));
+    }
+
+    public String findLongestCommonPrefixV1(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            // or throw IllegalArgumentException
+            return "";
+        }
+
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        int firstLen = strs[0].length();
+        for (int prefixLen = 0; prefixLen < firstLen; prefixLen++) {
+
+            char ch = strs[0].charAt(prefixLen);
+            for (int i = 1; i < strs.length; i++) {
+                if (prefixLen >= strs[i].length()
+                        || strs[i].charAt(prefixLen) != ch) {
+
+                    return strs[i].substring(0, prefixLen);
+                }
+            }
+        }
+
+        return strs[0];
+    }
+}
---
+++ src/main/java/com/example/javatoo/challenge/TransformStringWithStream.java
@@ -0,0 +1,24 @@
+package com.example.javatoo.challenge;
+
+import java.util.stream.Stream;
+
+public class TransformStringWithStream {
+    public static void main(String[] args) {
+
+        String result1Map = Stream.of("hello")
+                .map(s -> s + " world")
+                .findFirst()
+                .get();
+        System.out.println(result1Map);
+
+        String result2Map = Stream.of("gooool! ")
+                .map(String::toUpperCase)
+                .map(s -> s.repeat(2))
+                .map(s -> s.replaceAll("O", "OOOO"))
+                .findFirst()
+                .get();
+        System.out.println(result2Map);
+
+    }
+
+}
